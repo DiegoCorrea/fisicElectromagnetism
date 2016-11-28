16c3f4fee44d058d0426944052a55aed88090f84
diegocorrea <diegocorrea.cc@gmail.com>
2016-11-28 12:52:35 -0300

    start learn R

---
+++ tutorial/basicSyntax.R
@@ -0,0 +1,15 @@
+firstName <- "Ravi"
+lastName <- "Shankar"
+
+print(firstName)
+print(" ")
+print(lastName)
+
+if(FALSE) {
+  "This is a demo for multi-line comments and it should be put inside either a single 
+    OR double quote"
+}
+print(45 + 15)
+menor <- 0.15
+maior <- 0.45
+print(menor + maior)
