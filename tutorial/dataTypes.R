ef9e073f3a7024c1c0e248657f910c9d0f2b2b64
diegocorrea <diegocorrea.cc@gmail.com>
2016-12-05 14:08:04 -0300

    add linear graph

---
+++ tutorial/dataTypes.R
@@ -0,0 +1,19 @@
+print("Test is logical?")
+v <- TRUE 
+print(class(v))
+
+print(" ")
+print("Test is numeric?")
+v <- 23.5
+print(class(v))
+
+print(" ")
+print("Test is integer?")
+v <- 2L
+print(class(v))
+
+print(" ")
+print("Test is Complex?")
+v <- 2+5i
+print(class(v))
+
