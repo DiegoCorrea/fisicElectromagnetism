d9f9b3bb57f3ea5d85137410a8f8f33047c4ef41
diegocorrea <diegocorrea.cc@gmail.com>
2016-12-12 12:03:32 -0300

    load from csv

--- labs/bola_em_queda_livre/bola_em_queda_livre-F2(GerarPNG).R
+++ labs/bola_em_queda_livre/bola_em_queda_livre-F2(GerarPNG).R
@@ -1,7 +1,5 @@
 ###################################################################################################
-# Criando Vetor de Tempo Medio e Distância Media
-tempoMedio <- c(0.144,0.204,0.2485,0.2885,0.322,0.3535)
-distanciaMedia <- c(0.088,0.188,0.288,0.388,0.488,0.588)
+dados <- read.csv("bola_em_queda_livre-F2.csv", header = FALSE, col.names=c("tempoMedio", "distanciaMedia"))
 ###################################################################################################
 
 ###################################################################################################
@@ -31,14 +29,14 @@ aceleracaoMedia <- function(velocidadeMedia, tempoMedio){
 png(file = "bola_em_queda_livre-F2(TempoXDistancia).png")
 
 # Criando o Gráfico
-# tempoMedio é o eixo X
-# distanciaMedia é o eixo Y
+# dados$tempoMedio é o eixo X
+# dados$distanciaMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(tempoMedio, distanciaMedia, type="o", col="blue", 
+plot(dados$tempoMedio, dados$distanciaMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Tempo (segundos)",
@@ -53,7 +51,7 @@ dev.off()
 
 ###################################################################################################
 # Chamando a função que calcula a Velocidade media a partir da Distância Media e do Tempo Medio
-velocidadeMedia <- velocidadeMedia(distanciaMedia, tempoMedio)
+velocidadeMedia <- velocidadeMedia(dados$distanciaMedia, dados$tempoMedio)
 ###################################################################################################
 
 ###################################################################################################
@@ -65,13 +63,13 @@ png(file = "bola_em_queda_livre-F2(VelocidadexDistancia).png")
 
 # Criando o Gráfico
 # velocidadeMedia é o eixo X
-# distanciaMedia é o eixo Y
+# dados$distanciaMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(velocidadeMedia, distanciaMedia, type="o", col="blue", 
+plot(velocidadeMedia, dados$distanciaMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Velocidade (metros por segundos)",
@@ -92,13 +90,13 @@ png(file = "bola_em_queda_livre-F2(VelocidadexTempo).png")
 
 # Criando o Gráfico
 # velocidadeMedia é o eixo X
-# tempoMedio é o eixo Y
+# dados$tempoMedio é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(velocidadeMedia, tempoMedio, type="o", col="blue", 
+plot(velocidadeMedia, dados$tempoMedio, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Velocidade (metros por segundos)",
@@ -113,7 +111,7 @@ dev.off()
 
 ###################################################################################################
 # Chamando a função que calcula a Aceleração media a partir da Velocidade Media e do Tempo Medio
-aceleracaoMedia <- aceleracaoMedia(velocidadeMedia, tempoMedio)
+aceleracaoMedia <- aceleracaoMedia(velocidadeMedia, dados$tempoMedio)
 ###################################################################################################
 
 ###################################################################################################
@@ -124,14 +122,14 @@ aceleracaoMedia <- aceleracaoMedia(velocidadeMedia, tempoMedio)
 png(file = "bola_em_queda_livre-F2(DistanciaxAceleracao).png")
 
 # Criando o Gráfico
-# distanciaMedia é o eixo X
+# dados$distanciaMedia é o eixo X
 # aceleracaoMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(distanciaMedia, aceleracaoMedia, type="o", col="blue", 
+plot(dados$distanciaMedia, aceleracaoMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Distância (metros)",
@@ -178,14 +176,14 @@ dev.off()
 png(file = "bola_em_queda_livre-F2(TempoxAceleracao).png")
 
 # Criando o Gráfico
-# tempoMedio é o eixo X
+# dados$tempoMedio é o eixo X
 # aceleracaoMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(tempoMedio, aceleracaoMedia, type="o", col="blue", 
+plot(dados$tempoMedio, aceleracaoMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Tempo (segundos)",
