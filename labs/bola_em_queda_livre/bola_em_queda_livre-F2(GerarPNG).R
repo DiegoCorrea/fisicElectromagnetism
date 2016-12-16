803e55d266f8210f219c656a0c584fbbee11adaf
diegocorrea <diegocorrea.cc@gmail.com>
2016-12-16 02:03:22 -0300

    adicionado o Rmd

--- labs/bola_em_queda_livre/bola_em_queda_livre-F2(GerarPNG).R
+++ labs/bola_em_queda_livre/bola_em_queda_livre-F2(GerarPNG).R
@@ -1,25 +1,45 @@
 ###################################################################################################
-dados <- read.csv("bola_em_queda_livre-F2.csv", header = FALSE, col.names=c("tempoMedio", "distanciaMedia"))
+# Carregando dadosF2 do CSV que é separado por ","
+###################################################################################################
+dadosF2 <- read.csv("bola_em_queda_livre-F2.csv", header = FALSE, col.names=c("tempoInicial","tempoFinal","distanciaInicial","distanciaFinal"))
 ###################################################################################################
 
-###################################################################################################
-# Calculando Velocidade Media em cada queda
-###################################################################################################
+GRAVIDADE <- c(9.8,9.8,9.8,9.8,9.8,9.8)
 
-velocidadeMedia <- function(distanciaMedia, tempoMedio){
-  velocidade <- distanciaMedia/tempoMedio
-  return(velocidade)
+###################################################################################################
+# Calculando Velocidade Final em cada queda
+###################################################################################################
+calcularVelocidadeFinal <- function(velocidadeInicial, aceleracao, tempoFinal){
+  velocidadeFinal <- velocidadeInicial + (aceleracao*tempoFinal)
+  return(velocidadeFinal)
 }
 ###################################################################################################
 
 ###################################################################################################
 # Calculando Aceleração Media em cada queda
 ###################################################################################################
-
-aceleracaoMedia <- function(velocidadeMedia, tempoMedio){
-  aceleracao <- velocidadeMedia/tempoMedio
-  return(aceleracao)
+calcularAceleracaoMedia <- function(velocidadeFinal, velocidadeInicial, tempoFinal, tempoInicial){
+  aceleracaoMedia <- (velocidadeFinal - velocidadeInicial)/(tempoFinal - tempoInicial)
+  return(aceleracaoMedia)
 }
+###################################################################################################
+# Calculando o Tempo e Distância Media, e adicionando com nova coluna
+###################################################################################################
+dadosF2["tempoMedio"] <- dadosF2$tempoFinal - dadosF2$tempoInicial
+dadosF2["distanciaMedia"] <- dadosF2$distanciaFinal - dadosF2$distanciaInicial
+dadosF2["velocidadeInicial"] <- c(0,0,0,0,0,0)
+###################################################################################################
+
+###################################################################################################
+# Chamando a função que calcula a Velocidade media a partir da Distância Media e do Tempo Medio
+###################################################################################################
+dadosF2["velocidadeFinal"] <- calcularVelocidadeFinal(dadosF2$velocidadeInicial, GRAVIDADE, dadosF2$tempoFinal)
+###################################################################################################
+
+###################################################################################################
+# Chamando a função que calcula a Aceleração media a partir da Velocidade Media e do Tempo Medio
+###################################################################################################
+dadosF2["aceleracaoMedia"] <- calcularAceleracaoMedia(dadosF2$velocidadeFinal, dadosF2$velocidadeInicial, dadosF2$tempoFinal, dadosF2$tempoInicial)
 ###################################################################################################
 
 ###################################################################################################
@@ -29,14 +49,14 @@ aceleracaoMedia <- function(velocidadeMedia, tempoMedio){
 png(file = "bola_em_queda_livre-F2(TempoXDistancia).png")
 
 # Criando o Gráfico
-# dados$tempoMedio é o eixo X
-# dados$distanciaMedia é o eixo Y
+# dadosF2$tempoMedio é o eixo X
+# dadosF2$distanciaMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(dados$tempoMedio, dados$distanciaMedia, type="o", col="blue", 
+plot(dadosF2$tempoMedio, dadosF2$distanciaMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Tempo (segundos)",
@@ -50,11 +70,6 @@ dev.off()
 ###################################################################################################
 
 ###################################################################################################
-# Chamando a função que calcula a Velocidade media a partir da Distância Media e do Tempo Medio
-velocidadeMedia <- velocidadeMedia(dados$distanciaMedia, dados$tempoMedio)
-###################################################################################################
-
-###################################################################################################
 # Gerando Gráfico da Velocidade Media x Distância Media 
 ###################################################################################################
 
@@ -62,14 +77,14 @@ velocidadeMedia <- velocidadeMedia(dados$distanciaMedia, dados$tempoMedio)
 png(file = "bola_em_queda_livre-F2(VelocidadexDistancia).png")
 
 # Criando o Gráfico
-# velocidadeMedia é o eixo X
-# dados$distanciaMedia é o eixo Y
+# dadosF2$velocidadeFinal é o eixo X
+# dadosF2$distanciaMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(velocidadeMedia, dados$distanciaMedia, type="o", col="blue", 
+plot(dadosF2$velocidadeFinal, dadosF2$distanciaMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Velocidade (metros por segundos)",
@@ -89,29 +104,24 @@ dev.off()
 png(file = "bola_em_queda_livre-F2(VelocidadexTempo).png")
 
 # Criando o Gráfico
-# velocidadeMedia é o eixo X
-# dados$tempoMedio é o eixo Y
+# dadosF2$tempoMedio é o eixo x
+# dadosF2$velocidadeFinal é o eixo y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(velocidadeMedia, dados$tempoMedio, type="o", col="blue", 
+plot(dadosF2$tempoMedio, dadosF2$velocidadeFinal, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
-  xlab="Velocidade (metros por segundos)",
-  ylab="Tempo (segundos)")
+  xlab="Tempo (segundos)",
+  ylab="Velocidade (metros por segundos)")
 
 # grid cria quadrantes no gráfico, essas linhas da divisoria, terão a cor vermelha
 grid(col="red")
 
 # Salvando no png tudo que foi printado no gráfico
 dev.off()
-###################################################################################################
-
-###################################################################################################
-# Chamando a função que calcula a Aceleração media a partir da Velocidade Media e do Tempo Medio
-aceleracaoMedia <- aceleracaoMedia(velocidadeMedia, dados$tempoMedio)
 ###################################################################################################
 
 ###################################################################################################
@@ -122,14 +132,14 @@ aceleracaoMedia <- aceleracaoMedia(velocidadeMedia, dados$tempoMedio)
 png(file = "bola_em_queda_livre-F2(DistanciaxAceleracao).png")
 
 # Criando o Gráfico
-# dados$distanciaMedia é o eixo X
-# aceleracaoMedia é o eixo Y
+# dadosF2$distanciaMedia é o eixo X
+# dadosF2$aceleracaoMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(dados$distanciaMedia, aceleracaoMedia, type="o", col="blue", 
+plot(dadosF2$distanciaMedia, dadosF2$aceleracaoMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Distância (metros)",
@@ -149,14 +159,14 @@ dev.off()
 png(file = "bola_em_queda_livre-F2(VelocidadexAceleracao).png")
 
 # Criando o Gráfico
-# velocidadeMedia é o eixo X
-# aceleracaoMedia é o eixo Y
+# dadosF2$velocidadeFinal é o eixo X
+# dadosF2$aceleracaoMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(velocidadeMedia, aceleracaoMedia, type="o", col="blue", 
+plot(dadosF2$velocidadeFinal, dadosF2$aceleracaoMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Velocidade (metros por segundos)",
@@ -176,14 +186,14 @@ dev.off()
 png(file = "bola_em_queda_livre-F2(TempoxAceleracao).png")
 
 # Criando o Gráfico
-# dados$tempoMedio é o eixo X
-# aceleracaoMedia é o eixo Y
+# dadosF2$tempoMedio é o eixo X
+# dadosF2$aceleracaoMedia é o eixo Y
 # type "o" é o grafico contendo todos os pontos
 # main é o título do grafico, irá aparecer no centro, na parte superior
 # lwd é a espessura da linha do grafico
 # xlab é o título do eixo X
 # ylab é o título do eixo Y
-plot(dados$tempoMedio, aceleracaoMedia, type="o", col="blue", 
+plot(dadosF2$tempoMedio, dadosF2$aceleracaoMedia, type="o", col="blue", 
   main="Experimento 1: \nBola em queda livre \nF2",
   lwd=3,
   xlab="Tempo (segundos)",
@@ -194,3 +204,5 @@ grid(col="red")
 
 # Salvando no png tudo que foi printado no gráfico
 dev.off()
+
+print(dadosF2)
